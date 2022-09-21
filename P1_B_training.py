53ec8aedc9d362d99a6a905c8e259f5ddc02b37f
Li, Sheng-Wei <pha123661@gmail.com>
2022-09-21 13:48:25 +0800

    Tuning

--- P1_B_training.py
+++ P1_B_training.py
@@ -4,9 +4,10 @@ import shutil
 import numpy as np
 import timm
 import torch
-import torchvision.transforms as trns
 from sklearn.metrics import accuracy_score
+from timm.data import resolve_data_config
 from timm.data.mixup import Mixup
+from timm.data.transforms_factory import create_transform
 from timm.loss import SoftTargetCrossEntropy
 from torch import nn
 from torch.utils.data import DataLoader
@@ -14,36 +15,7 @@ from torch.utils.tensorboard import SummaryWriter
 
 from P1_dataloader import p1_dataset
 
-# load data
-mean = [0.5077, 0.4813, 0.4312]  # calculated at dataloader.py
-std = [0.2000, 0.1986, 0.2034]
-train_dataset = p1_dataset(
-    '/shared_home/r11944004/pepper_local_disk/DLCV/hw1-pha123661/hw1_data/hw1_data/p1_data/train_50',
-    trns.Compose([
-        trns.Resize((256, 256)),
-        trns.RandomCrop((224, 224)),  # pre-trained model works on 224*224
-        trns.RandomHorizontalFlip(),
-        trns.ToTensor(),
-        trns.Normalize(mean=mean, std=std),
-    ])
-)
-
-valid_dataset = p1_dataset(
-    '/shared_home/r11944004/pepper_local_disk/DLCV/hw1-pha123661/hw1_data/hw1_data/p1_data/val_50',
-    trns.Compose([
-        trns.Resize((224, 224)),
-        trns.ToTensor(),
-        trns.Normalize(mean=mean, std=std),
-    ])
-)
-
-
-train_loader = DataLoader(
-    dataset=train_dataset, batch_size=8, shuffle=True, num_workers=4, pin_memory=True)
-valid_loader = DataLoader(
-    dataset=valid_dataset, batch_size=8, shuffle=False, num_workers=4, pin_memory=True)
-
-model = 'beit_base_patch16_224_in22k'
+model = 'resnetv2_101x1_bitm'
 device = torch.device('cuda')
 epochs = 1000
 plot_epochs = {100 * i for i in range(1, 11)} | {1}
@@ -60,9 +32,10 @@ net.train()
 loss_fn = SoftTargetCrossEntropy()
 optim = torch.optim.AdamW(net.parameters(), lr=0.001)
 mixup = Mixup(
-    mixup_alpha=0.8,
-    cutmix_alpha=0.8,
-    prob=0.3,
+    mixup_alpha=0.1,
+    cutmix_alpha=1,
+    cutmix_minmax=None,
+    prob=1,
     switch_prob=0.5,
     mode='batch',
     label_smoothing=0.1,
@@ -72,17 +45,32 @@ mixup = Mixup(
 if not os.path.isdir(ckpt_path):
     os.mkdir(ckpt_path)
 
+config = resolve_data_config({}, model=model)
+transform = create_transform(**config)
+train_dataset = p1_dataset(
+    '/shared_home/r11944004/pepper_local_disk/DLCV/hw1-pha123661/hw1_data/hw1_data/p1_data/train_50',
+    transform
+)
+
+valid_dataset = p1_dataset(
+    '/shared_home/r11944004/pepper_local_disk/DLCV/hw1-pha123661/hw1_data/hw1_data/p1_data/val_50',
+    transform
+)
+
+
+train_loader = DataLoader(
+    dataset=train_dataset, batch_size=64, shuffle=True, num_workers=4, pin_memory=True)
+valid_loader = DataLoader(
+    dataset=valid_dataset, batch_size=64, shuffle=False, num_workers=4, pin_memory=True)
+
 # train
-global_step = 0
 for epoch in range(1, epochs + 1):
     for x, y in train_loader:
-        global_step += 1
         x, y = x.to(device, non_blocking=True), y.to(
             device, non_blocking=True)
         x, y = mixup(x, y)
-
         optim.zero_grad()
-        logits = net(x)  # no need to calculate soft-max
+        logits = net(x)  # no need to calculate soft-max%
         loss = loss_fn(logits, y)
         loss.backward()
         optim.step()
@@ -102,8 +90,8 @@ for epoch in range(1, epochs + 1):
         va_loss = va_loss / len(valid_loader)
         va_acc /= len(valid_loader)
     net.train()
-    writer.add_scalar('validation/acc', va_acc, global_step)
-    writer.add_scalar('validation/loss', va_loss, global_step)
+    writer.add_scalar('validation/acc', va_acc, epoch - 1)
+    writer.add_scalar('validation/loss', va_loss, epoch - 1)
 
     print(f"epoch {epoch}, va_acc = {va_acc}, va_loss = {va_loss}")
     if va_acc > best_acc:
