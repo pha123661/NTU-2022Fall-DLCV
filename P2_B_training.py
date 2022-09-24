7482d66ba81cb090b21494e41ad66e234b0c3151
Li, Sheng-Wei <pha123661@gmail.com>
2022-09-24 16:29:18 +0800

    Fix grad accum bug

--- P2_B_training.py
+++ P2_B_training.py
@@ -57,7 +57,7 @@ if not os.path.isdir(ckpt_path):
 
 
 for epoch in range(1, epochs + 1):
-    for x, y in tqdm(train_loader):
+    for idx, (x, y) in enumerate(tqdm(train_loader), 1):
         x, y = x.to(device, non_blocking=True), y.to(device, non_blocking=True)
         outs = net(x)
         loss = 0
@@ -66,38 +66,37 @@ for epoch in range(1, epochs + 1):
         loss /= accum_steps
         loss /= len(outs)
         loss.backward()
-        if (epoch % accum_steps) == 0 or (epoch == epochs):
+        if (idx % accum_steps) == 0 or (idx == len(train_loader)):
             optim.step()
             optim.zero_grad()
 
-    if (epoch % accum_steps) == 0 or (epoch == epochs):
-        net.eval()
-        with torch.no_grad():
-            va_loss = 0
-            mIOUs = []
-            for x, y in tqdm(valid_loader):
-                x, y = x.to(device), y.to(device)
-                out = net(x)
-                pred = out.argmax(dim=1)
-                va_loss += nn.functional.cross_entropy(out, y).item()
+    net.eval()
+    with torch.no_grad():
+        va_loss = 0
+        mIOUs = []
+        for x, y in tqdm(valid_loader):
+            x, y = x.to(device), y.to(device)
+            out = net(x)
+            pred = out.argmax(dim=1)
+            va_loss += nn.functional.cross_entropy(out, y).item()
 
-                pred = pred.detach().cpu().numpy().astype(np.int64)
-                y = y.detach().cpu().numpy().astype(np.int64)
-                for p, gt in zip(pred, y):
-                    mIOUs.append(jaccard_score(
-                        gt.flatten(), p.flatten(), average='macro'))
+            pred = pred.detach().cpu().numpy().astype(np.int64)
+            y = y.detach().cpu().numpy().astype(np.int64)
+            for p, gt in zip(pred, y):
+                mIOUs.append(jaccard_score(
+                    gt.flatten(), p.flatten(), average='macro'))
 
-            va_loss /= len(valid_loader)
-            mIOU = sum(mIOUs) / len(mIOUs)
-        net.train()
-        print(f"epoch {epoch}, mIOU = {mIOU}, va_loss = {va_loss}")
-        if va_loss <= best_loss:
-            best_loss = va_loss
-            torch.save(optim.state_dict(), os.path.join(
-                ckpt_path, 'best_optimizer.pth'))
-            torch.save(net.state_dict(), os.path.join(
-                ckpt_path, 'best_model.pth'))
-            print("new model saved sucessfully!")
+        va_loss /= len(valid_loader)
+        mIOU = sum(mIOUs) / len(mIOUs)
+    net.train()
+    print(f"epoch {epoch}, mIOU = {mIOU}, va_loss = {va_loss}")
+    if va_loss <= best_loss:
+        best_loss = va_loss
+        torch.save(optim.state_dict(), os.path.join(
+            ckpt_path, 'best_optimizer.pth'))
+        torch.save(net.state_dict(), os.path.join(
+            ckpt_path, 'best_model.pth'))
+        print("new model saved sucessfully!")
 
     if (epoch % 10) == 0 or epoch == 1:
         torch.save(net.state_dict(), os.path.join(
