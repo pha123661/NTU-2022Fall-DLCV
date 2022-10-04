cb27d187b9338cd3498d8b7a308677f008135128
Li, Sheng-Wei <pha123661@gmail.com>
2022-10-04 15:22:09 +0800

    Update viz gt

--- viz.sh
+++ viz.sh
@@ -1,3 +1,4 @@
+# 3 models
 for filename in "0013_sat" "0062_sat" "0104_sat"
 do
     for folder in "first" "mid" "last"
@@ -5,4 +6,11 @@ do
         python3 viz_mask.py --img_path="./p2 plot fig/$filename.jpg" --seg_path="./p2 plot fig/$folder/$filename.png"
         mv exp.png "./p2 plot fig/$folder/$filename rst.png"
     done
+done
+
+# ground truth
+for filename in "0013" "0062" "0104"
+do
+    python3 viz_mask.py --img_path="./p2 plot fig/${filename}_sat.jpg" --seg_path="./p2 plot fig/${filename}_mask.png"
+    mv exp.png "./p2 plot fig/$filename gt.png"
 done
