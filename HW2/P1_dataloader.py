d0c93a836aa084a61f58d88d8c5ec6310edcefb8
Li, Sheng-Wei <pha123661@gmail.com>
2022-10-11 14:34:16 +0800

    Add dataloader

---
+++ HW2/P1_dataloader.py
@@ -0,0 +1,43 @@
+import glob
+import os
+
+import torch
+from PIL import Image
+from torch.utils.data import Dataset
+from torchvision import transforms
+from torchvision.datasets import ImageFolder
+
+
+class p1_dataset(Dataset):
+    def __init__(self, root, transform) -> None:
+        super().__init__()
+
+        self.Transform = transform
+        self.Image_names = glob.glob(os.path.join(root, "*.png"))
+
+    def __getitem__(self, idx):
+        img = Image.open(self.Image_names[idx])
+        img = self.Transform(img)
+
+        return img
+
+    def __len__(self):
+        return len(self.Image_names)
+
+
+if __name__ == '__main__':
+    dataset = p1_dataset(
+        root='./hw2_data/face/train',
+        transform=transforms.Compose([
+            transforms.ToTensor(),
+        ])
+    )
+    mean = torch.zeros(3)
+    std = torch.zeros(3)
+    for x in dataset:
+        mean += x.mean(dim=(1, 2))
+        std += x.std(dim=(1, 2))
+
+    mean /= len(dataset)
+    std /= len(dataset)
+    print(mean, std)
