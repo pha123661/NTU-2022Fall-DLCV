b8976e7171c56d20a4fd9b76fc0be44b24767111
Li, Sheng-Wei <pha123661@gmail.com>
2022-10-11 22:37:54 +0800

    Update P1

--- HW2/P1_A_inference.py
+++ HW2/P1_A_inference.py
@@ -1,41 +1,63 @@
 import argparse
 import pathlib
+import random
 
 import torch
 from torchvision.utils import save_image
 
 from P1_model import DCGAN_G
 
-parser = argparse.ArgumentParser()
-parser.add_argument(
-    "-w", "--weight",
-    help="path to generator weight",
-    type=pathlib.Path
-)
-parser.add_argument(
-    "-o", "--out_dir",
-    help="output directory",
-    type=pathlib.Path,
-    default='./P1_A_out',
-)
-parser.add_argument(
-    "-d", "--device",
-    help="device",
-    type=torch.device,
-    default='cuda' if torch.cuda.is_available() else 'cpu',
-)
-args = parser.parse_args()
 
-args.out_dir.mkdir(exist_ok=True)
+def main(device, weight, out_dir):
+    random.seed(0)
+    torch.manual_seed(0)
 
-batch_size = 100
-model = DCGAN_G().to(args.device)
-model.load_state_dict(torch.load(args.weight, map_location=args.device))
+    batch_size = 100
+    model = DCGAN_G().to(device)
+    model.load_state_dict(torch.load(weight, map_location=device))
 
-idx = 0
-for i in range(1000 // batch_size):
-    z = torch.randn(batch_size, 100, 1, 1, device=args.device)
-    gen_imgs = model(z)
-    for img in gen_imgs:
-        save_image(img, args.out_dir / f'{idx}.png', normalize=True)
-        idx += 1
+    idx = 0
+    for _ in range(1000 // batch_size):
+        z = torch.randn(batch_size, 100, 1, 1, device=device)
+        gen_imgs = model(z)
+        for img in gen_imgs:
+            save_image(img, out_dir / f'{idx}.png', normalize=True)
+            idx += 1
+
+    # FID = fid_score.calculate_fid_given_paths(
+    #     [str(out_dir), 'hw2_data/face/val'],
+    #     batch_size=batch_size,
+    #     device=device,
+    #     dims=2048,
+    #     num_workers=8,
+    # )
+    # print(f"FID: {FID}")
+    # return FID
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-w", "--weight",
+        help="path to generator weight",
+        type=pathlib.Path,
+        required=True,
+    )
+    parser.add_argument(
+        "-o", "--out_dir",
+        help="output directory",
+        type=pathlib.Path,
+        default='./P1_A_out',
+    )
+    parser.add_argument(
+        "-d", "--device",
+        help="device",
+        type=torch.device,
+        default='cuda' if torch.cuda.is_available() else 'cpu',
+    )
+    return parser.parse_args()
+
+
+if __name__ == '__main__':
+    args = parse_args()
+    args.out_dir.mkdir(exist_ok=True)
--- HW2/P1_A_training.py
+++ HW2/P1_A_training.py
@@ -1,15 +1,47 @@
+import random
 from pathlib import Path
 
 import torch
 import torch.nn as nn
+from pytorch_fid import fid_score
 from torch.utils.data import DataLoader
 from torch.utils.tensorboard.writer import SummaryWriter
 from torchvision import transforms
-from torchvision.utils import make_grid
+from torchvision.utils import make_grid, save_image
 from tqdm import tqdm
 
 from P1_dataloader import p1_dataset
 from P1_model import DCGAN_D, DCGAN_G
+
+# def get_FID(device, weight, out_dir):
+#     state_1, state_2 = random.getstate(), torch.get_rng_state()
+#     random.seed(0)
+#     torch.manual_seed(0)
+
+#     batch_size = 100
+#     model = DCGAN_G().to(device)
+#     model.load_state_dict(torch.load(weight, map_location=device))
+
+#     idx = 0
+#     for _ in range(1000 // batch_size):
+#         with torch.no_grad():
+#             z = torch.randn(batch_size, 100, 1, 1, device=device)
+#             gen_imgs = model(z)
+#             for img in gen_imgs:
+#                 save_image(img, out_dir / f'{idx}.png', normalize=True)
+#                 idx += 1
+
+#     FID = fid_score.calculate_fid_given_paths(
+#         [str(out_dir), 'hw2_data/face/val'],
+#         batch_size=batch_size,
+#         device=device,
+#         dims=2048,
+#         num_workers=8,
+#     )
+
+#     random.setstate(state_1)
+#     torch.set_rng_state(state_2)
+#     return FID
 
 
 def rm_tree(pth: Path):
@@ -27,13 +59,7 @@ std = [0.2513, 0.2157, 0.1997]
 train_set = p1_dataset(
     root='./hw2_data/face/train',
     transform=transforms.Compose([
-        transforms.ToTensor(),
-        transforms.Normalize(mean, std),
-    ])
-)
-val_set = p1_dataset(
-    root='./hw2_data/face/val',
-    transform=transforms.Compose([
+        transforms.RandomHorizontalFlip(),
         transforms.ToTensor(),
         transforms.Normalize(mean, std),
     ])
@@ -42,8 +68,6 @@ val_set = p1_dataset(
 batch_size = 128
 train_loader = DataLoader(
     train_set, batch_size=batch_size, shuffle=True, num_workers=6)
-valid_loader = DataLoader(
-    val_set, batch_size=2 * batch_size, shuffle=False, num_workers=6)
 
 
 num_epochs = 10
@@ -54,7 +78,7 @@ tb_path = Path('./P1_A_tb')
 rm_tree(ckpt_path)
 rm_tree((tb_path))
 
-plot_K_iters = 100
+plot_K_iters = 200
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 loss_fn = nn.BCELoss()  # take y as 1 or 0 <-> choosing logx or log(1-x) in BCE
@@ -63,7 +87,7 @@ model_G = DCGAN_G().to(device)  # z.shape = (b, 100, 1, 1)
 model_D = DCGAN_D().to(device)
 
 optim_G = torch.optim.Adam(model_G.parameters(), lr=lr, betas=(0.5, 0.999))
-optim_D = torch.optim.Adam(model_D.parameters(), lr=lr, betas=(0.5, 0.999))
+optim_D = torch.optim.Adam(model_D.parameters(), lr=lr)
 
 writer = SummaryWriter(tb_path)
 
@@ -71,9 +95,10 @@ writer = SummaryWriter(tb_path)
 plot_noise = torch.randn(64, 100, 1, 1, device=device)
 iters = 0
 ckpt_path.mkdir(exist_ok=True)
+best_FID = 10e10
+best_ITER = -1
 for epoch in range(1, num_epochs + 1):
-    pbar = tqdm(train_loader)
-    for real_imgs in pbar:
+    for real_imgs in train_loader:
         '''
         Update Discriminator:
         maximizes log(D(x)) + log(1 - D(G(z)))
@@ -120,15 +145,26 @@ for epoch in range(1, num_epochs + 1):
         loss_G.backward()
         optim_G.step()
 
-        pbar.set_description(
-            f"D(x): {L_D:.4f}, D(G(z)): {L_G_1:.4f}, {L_G_2:.4f}")
+        writer.add_scalar('D(x)', L_D, global_step=iters)
+        writer.add_scalar('D(G(z)) 1', L_G_1, global_step=iters)
+        writer.add_scalar('D(G(z)) 2', L_G_2, global_step=iters)
 
-        if (iters % plot_K_iters) == 0:
+        if epoch > 1 and (iters % plot_K_iters) == 0:
             with torch.no_grad():
                 plot_img = model_G(plot_noise).detach().cpu()
                 grid = make_grid(plot_img, padding=2, normalize=True)
                 writer.add_image('Generated results', grid, iters)
             torch.save(model_G.state_dict(), ckpt_path / f"{iters}_G.pth")
             torch.save(model_D.state_dict(), ckpt_path / f"{iters}_D.pth")
-            print('new result saved!')
+            # FID = get_FID(device=device, weight=ckpt_path /
+            #               f"{iters}_G.pth", out_dir=Path('./P1_A_out'))
+            # if FID <= best_FID:
+            #     best_FID = FID
+            #     best_ITER = iters
+            #     print(f"[NEW] ITER {iters} BEST FID: {FID}")
+            # else:
+            #     print(f"[BAD] ITER {iters} FID: {FID}, BEST FID: {best_FID}")
+
         iters += 1
+
+print(f"[RST] iter {best_ITER}, best FID: {best_FID}")
