62b4bee20fbd1bcdb5b31efe7673103776439cc4
Li, Sheng-Wei <pha123661@gmail.com>
2022-10-23 16:16:38 +0800

    Add download script

---
+++ HW2/hw2_download.sh
@@ -0,0 +1,4 @@
+gdown --folder https://drive.google.com/drive/folders/1g04gnoseb7Ni9jOPAxobFcuewja0wP9M?usp=sharing
+gdown --folder https://drive.google.com/drive/folders/1bulO-jTrfSkMgXecAcswDWDK5rUJA1mq?usp=sharing
+gdown --folder https://drive.google.com/drive/folders/1dv_wiq9_OU9seC1UPygrW07ep0BXgt3B?usp=sharing
+gdown --folder https://drive.google.com/drive/folders/1la43cxg21dUAaRCyK9fZWIarJjFelqxC?usp=sharing
