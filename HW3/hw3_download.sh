f80170e681e999e91ff20b1181d80f5b5d391458
Li, Sheng-Wei <pha123661@gmail.com>
2022-11-13 20:33:00 +0800

    Update download

--- HW3/hw3_download.sh
+++ HW3/hw3_download.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
+gdown --folder https://drive.google.com/drive/folders/1n5rza7fW-AHxUc4JO4DWOBwLOxofWIj7?usp=sharing
 gdown --folder https://drive.google.com/drive/folders/18zVD_afRmTll44S91EOkB20ow03flDF7?usp=sharing
+python3 P2_download.py
