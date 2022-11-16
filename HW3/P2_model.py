4a04defceb491effddfbfa3103b5b5a3abbaedbb
Li, Sheng-Wei <pha123661@gmail.com>
2022-11-16 23:07:04 +0800

    Add beam search

--- HW3/P2_model.py
+++ HW3/P2_model.py
@@ -1,9 +1,9 @@
 import math
-import random
 
 import timm
 import torch
 from torch import nn
+from torch.utils.data import DataLoader, TensorDataset
 
 
 class ImageCaptioningTransformer(nn.Module):
@@ -131,6 +131,128 @@ class ImageCaptioningTransformer(nn.Module):
             current_state = torch.concat((current_state, next_word), dim=-1)
         return current_state[0, 1:].cpu().tolist()  # remove [BOS]
 
+    @torch.no_grad()
+    def batch_beam_search(self, img, beams=3, max_length=30):
+        def forward_prob(memory, input_ids):
+            in_embed = self.word_embedding(input_ids)
+            in_embed += self.positional_embedding(in_embed)
+            logits = self.decoder(tgt=in_embed, memory=memory)
+            logits = self.head(logits[:, -1, :])
+            return logits
+
+        self.eval()
+        if img.dim() < 4:
+            img = img.unsqueeze(0)
+        memory = self.encoder.forward_features(img)
+
+        device = memory.device
+        current_state = self.BOS_Token * \
+            torch.ones(memory.shape[0], 1, dtype=torch.long).to(device)
+
+        next_probs = forward_prob(memory, current_state)
+        vocab_size = next_probs.shape[-1]
+        probs, next_chars = \
+            next_probs.reshape(next_probs.shape[0], next_probs.shape[-1])\
+            .log_softmax(-1)\
+            .topk(k=beams, axis=-1)  # shape = (B, topk)
+        current_state = current_state.repeat((beams, 1))
+        next_chars = next_chars.reshape(-1, 1)
+        current_state = torch.cat((current_state, next_chars), axis=1)
+        memory = memory.repeat((beams, 1, 1))
+
+        for i in range(2, max_length + 1):
+            next_probs = forward_prob(memory, current_state).log_softmax(-1)
+            next_probs = next_probs.reshape((-1, beams, next_probs.shape[-1]))
+            probs = probs.unsqueeze(-1) + next_probs
+            probs = probs.flatten(start_dim=1)
+            probs, idx = probs.topk(k=beams, axis=-1)
+            next_chars = torch.remainder(
+                idx, vocab_size).flatten().unsqueeze(-1)
+            top_candidates = (idx / vocab_size).long()
+            top_candidates += torch.arange(current_state.shape[0] //
+                                           beams, device=device).unsqueeze(-1) * beams
+            current_state = current_state[top_candidates].flatten(end_dim=-2)
+            current_state = torch.cat([current_state, next_chars], axis=1)
+
+        current_state = current_state.reshape(-1,
+                                              beams, current_state.shape[-1])
+        current_state = current_state[..., 1:]  # remove BOS
+        # get best Y
+        best_idx = probs.argmax(-1)
+        current_state = current_state[torch.arange(len(best_idx)), best_idx, :]
+        exit()
+        return current_state.cpu().tolist()
+
+    @torch.no_grad()
+    def beam_search(self, img, beams=3, max_length=30):
+        def forward_prob(memory, input_ids):
+            in_embed = self.word_embedding(input_ids)
+            in_embed += self.positional_embedding(in_embed)
+            logits = self.decoder(tgt=in_embed, memory=memory)
+            logits = self.head(logits[:, -1, :])
+            return logits
+
+        self.eval()
+        if img.dim() < 4:
+            img = img.unsqueeze(0)
+        memory = self.encoder.forward_features(img)
+        device = memory.device
+        current_state = torch.tensor([self.BOS_Token]).to(device).unsqueeze(1)
+
+        # get top k words
+        next_probs = forward_prob(memory, current_state)
+        vocab_size = next_probs.shape[-1]
+        current_probs, next_chars = next_probs.log_softmax(
+            -1).topk(k=beams, axis=-1)
+        current_probs = current_probs.reshape(beams)
+        next_chars = next_chars.reshape(beams, 1)
+
+        # gen first k beams
+        current_state = current_state.repeat((beams, 1))
+        current_state = torch.cat((current_state, next_chars), axis=1)
+
+        ans_ids = []
+        ans_probs = []
+        for i in range(max_length - 1):
+            # get top k beams for beam*beam candidates
+            next_probs = forward_prob(memory.repeat(
+                (beams, 1, 1)), current_state).log_softmax(-1)
+            current_probs = current_probs.unsqueeze(-1) + next_probs
+            current_probs = current_probs.flatten()  # (beams*vocab)
+
+            # length normalization
+            _, idx = (current_probs / (len(current_state[0]) + 1))\
+                .topk(k=beams, dim=-1)
+            current_probs = current_probs[idx]
+
+            # get corresponding next char
+            next_chars = torch.remainder(idx, vocab_size)
+            next_chars = next_chars.unsqueeze(-1)
+
+            # get corresponding original beams
+            top_candidates = (idx / vocab_size).long()
+            current_state = current_state[top_candidates]
+            current_state = torch.cat((current_state, next_chars), dim=1)
+
+            # concat next_char to beams
+            to_rm_idx = set()
+            for idx, ch in enumerate(next_chars):
+                if i == (max_length - 2) or ch.item() == self.EOS_Token:
+                    ans_ids.append(current_state[idx].cpu().tolist())
+                    ans_probs.append(
+                        current_probs[idx].item() / len(ans_ids[-1]))
+                    to_rm_idx.add(idx)
+                    beams -= 1
+            to_keep_idx = [i for i in range(
+                len(current_state)) if i not in to_rm_idx]
+            if len(to_keep_idx) == 0:
+                break
+            current_state = current_state[to_keep_idx]
+            current_probs = current_probs[to_keep_idx]
+
+        max_idx = torch.argmax(torch.tensor(ans_probs)).item()
+        return ans_ids[max_idx]
+
 
 class PositionalEmbedding(nn.Module):
 
@@ -185,6 +307,6 @@ if __name__ == "__main__":
 
     td = DataLoader(train_set, 8, collate_fn=train_set.collate_fn)
     data = next(iter(td))
-    seq = Transformer.batch_greedy_search(data['images'], max_length=6)
+    seq = Transformer.Beam_search(
+        data['images'], max_length=6, return_probs=True)
     print(seq)
-    print(tokenizer.decode_batch(seq))
