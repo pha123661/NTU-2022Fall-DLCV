0dae76971c8f6cfa0ef912c0bae1a110e1384c8f
Li, Sheng-Wei <pha123661@gmail.com>
2022-11-17 14:29:10 +0800

    Fix: python->python3

--- HW3/hw3_download.sh
+++ HW3/hw3_download.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 gdown --folder https://drive.google.com/drive/folders/18zVD_afRmTll44S91EOkB20ow03flDF7?usp=sharing
-python -c "import clip; clip.load('ViT-L/14')"
+python3 -c "import clip; clip.load('ViT-L/14')"
