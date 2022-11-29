47c7c304625f2672179add4d56f18889fd183826
Li, Sheng-Wei <pha123661@gmail.com>
2022-11-29 16:52:49 +0800

    Update: add training script to train classifier

---
+++ HW4/P2_finetuning.py
@@ -0,0 +1,205 @@
+import argparse
+import json
+import pathlib
+import shutil
+
+import torch
+from torch.utils.data import DataLoader
+from torch.utils.tensorboard.writer import SummaryWriter
+from torchvision import models, transforms
+from tqdm import tqdm
+
+from P2_dataloader import ImageFolderDataset
+from P2_model import Classifier
+from warmup_scheduler import GradualWarmupScheduler
+
+
+def main(args):
+    train_transform = transforms.Compose([
+        transforms.Resize((128, 128)),
+        transforms.ToTensor(),
+        transforms.Normalize(
+            mean=[0.485, 0.456, 0.406],
+            std=[0.229, 0.224, 0.225]
+        )
+    ])
+    val_transform = transforms.Compose([
+        transforms.Resize((128, 128)),
+        transforms.ToTensor(),
+        transforms.Normalize(
+            mean=[0.485, 0.456, 0.406],
+            std=[0.229, 0.224, 0.225]
+        )
+    ])
+
+    train_set = ImageFolderDataset(
+        args.train_image_dir,
+        transform=train_transform,
+        label_csv=args.train_label,
+    )
+    val_set = ImageFolderDataset(
+        args.val_image_dir,
+        transform=val_transform,
+        label_csv=args.val_label,
+    )
+    train_loader = DataLoader(
+        train_set, batch_size=args.batch_size, shuffle=True, num_workers=6, pin_memory=True
+    )
+    val_loader = DataLoader(
+        val_set, batch_size=args.batch_size * 2, shuffle=False, num_workers=4, pin_memory=True
+    )
+
+    backbone = models.resnet50(weights=None)
+    backbone.load_state_dict(torch.load(
+        args.backbone, map_location=args.device))
+    backbone = backbone.to(args.device)
+    if args.freeze_backbone:
+        for p in backbone.parameters():
+            p.requires_grad = False
+
+    model = Classifier(
+        backbone=backbone,
+        in_features=1000,
+        n_class=len(train_set.label2idx),
+        n_layers=args.n_layers,
+        dropout=args.dropout,
+        hidden_size=args.hidden_size,
+    ).to(args.device)
+
+    # Training
+    amp_enable = any([args.fp16, args.bf16])
+    amp_dtype = torch.bfloat16 if args.bf16 else torch.float16
+    amp_device_type = 'cpu' if args.device == torch.device('cpu') else 'cuda'
+    if amp_enable:
+        print(
+            f"Enable AMP training using dtype={amp_dtype} on {amp_device_type}")
+
+    criterion = torch.nn.CrossEntropyLoss()
+    optim = torch.optim.Adam(model.parameters(), lr=args.lr)
+    scaler = torch.cuda.amp.GradScaler(enabled=amp_enable)
+    scheduler = GradualWarmupScheduler(
+        optim,
+        multiplier=1,
+        total_epoch=args.warmup_steps,
+        after_scheduler=torch.optim.lr_scheduler.CosineAnnealingLR(
+            optim,
+            T_max=args.epochs * len(train_loader) - args.warmup_steps
+        )
+    )
+
+    # Logging
+    if args.tensorboard_path.exists():
+        shutil.rmtree(args.tensorboard_path)
+    writer = SummaryWriter(args.tensorboard_path)
+    log_global_step = 0
+    history_best = 0
+    optim.zero_grad(set_to_none=True)
+    optim.step()
+
+    for epoch in range(args.epochs):
+        # Training
+        for data in tqdm(train_loader):
+            # Forward & Backpropagation
+            img = data['img'].to(args.device)
+            label = data['label'].to(args.device)
+            with torch.autocast(device_type=amp_device_type, dtype=amp_dtype, enabled=amp_enable):
+                logits = model(img)
+                loss = criterion(logits, label)
+
+            # Update
+            scheduler.step()
+            optim.zero_grad(set_to_none=True)
+            scaler.scale(loss).backward()
+            scaler.step(optim)
+            scaler.update()
+
+            # Log
+            writer.add_scalar("training/lr",
+                              optim.param_groups[0]['lr'], global_step=log_global_step)
+            writer.add_scalar("training/loss", loss.item(),
+                              global_step=log_global_step)
+
+            log_global_step += 1
+
+        # Validation
+        model.eval()
+        va_loss = []
+        va_acc = []
+        for data in val_loader:
+            img = data['img'].to(args.device)
+            label = data['label'].to(args.device)
+            with torch.no_grad():
+                with torch.autocast(device_type=amp_device_type, dtype=amp_dtype, enabled=amp_enable):
+                    logits = model(img)
+                    va_loss.append(torch.nn.functional.cross_entropy(
+                        logits, label).item())
+            y_pred = torch.argmax(logits, dim=1)
+            va_acc.append(torch.mean(
+                (y_pred == label).type(torch.float)).item())
+
+        va_loss = sum(va_loss) / len(va_loss)
+        va_acc = sum(va_acc) / len(va_acc)
+        writer.add_scalar("validation/loss", va_loss,
+                          global_step=log_global_step)
+        writer.add_scalar("validation/accuracy", va_acc,
+                          global_step=log_global_step)
+        model.train()
+
+        print(f"epoch {epoch}: va acc = {va_acc}")
+        if va_acc > history_best:
+            # Save
+            history_best = va_acc
+            torch.save(model.state_dict(), args.ckpt_dir /
+                       f"best_classifier.pth")
+            print('## Saved model')
+
+    training_args_dict = {k: str(v) for k, v in vars(args).items()}
+    print(f"## Training args: {json.dumps(training_args_dict, indent=2)}")
+    print(f"## Final Result, va acc = {va_acc}")
+
+
+def parse():
+    parser = argparse.ArgumentParser()
+
+    # Environment
+    parser.add_argument('--train_image_dir', type=pathlib.Path,
+                        default='hw4_data/office/train')
+    parser.add_argument('--train_label', type=pathlib.Path,
+                        default='hw4_data/office/train.csv')
+    parser.add_argument('--val_image_dir', type=pathlib.Path,
+                        default='hw4_data/office/val')
+    parser.add_argument('--val_label', type=pathlib.Path,
+                        default='hw4_data/office/val.csv')
+    parser.add_argument('--device', type=torch.device,
+                        default='cuda' if torch.cuda.is_available() else 'cpu')
+
+    parser.add_argument('--backbone', type=pathlib.Path,
+                        default='hw4_data/pretrain_model_SL.pt')
+
+    # Output Path
+    parser.add_argument("--tensorboard_path",
+                        type=pathlib.Path, default="./P2_tb/finetune")
+    parser.add_argument("--ckpt_dir",
+                        type=pathlib.Path, default="./P2_ckpt")
+
+    # Training args
+    parser.add_argument("--fp16", action="store_true")
+    parser.add_argument("--bf16", action="store_true")
+    parser.add_argument('--freeze_backbone', action='store_true')
+    parser.add_argument("--epochs", type=int, default=100)
+    parser.add_argument("--lr", type=float, default=5e-4)
+    parser.add_argument("--batch_size", type=int, default=64)
+    parser.add_argument("--warmup_steps", type=int, default=300)
+
+    # Model args
+    parser.add_argument('--n_layers', type=int, default=3)
+    parser.add_argument('--dropout', type=float, default=0.1)
+    parser.add_argument('--hidden_size', type=int, default=512)
+    args = parser.parse_args()
+    return args
+
+
+if __name__ == '__main__':
+    args = parse()
+    args.ckpt_dir.mkdir(exist_ok=True, parents=True)
+    main(args)
---
+++ HW4/P2_model.py
@@ -0,0 +1,27 @@
+import torch
+from torch import nn
+
+
+class Classifier(nn.Module):
+    def __init__(self, backbone, in_features, n_class, n_layers=3, dropout=0.1, hidden_size=512) -> None:
+        super().__init__()
+        layers = []
+        layers.extend([
+            nn.Linear(in_features, hidden_size),
+            nn.LeakyReLU(0.2),
+            nn.Dropout(dropout),
+        ])
+        for _ in range(n_layers - 2):
+            layers.extend([
+                nn.Linear(hidden_size, hidden_size),
+                nn.LeakyReLU(0.2),
+                nn.Dropout(dropout),
+            ])
+        layers.append(nn.Linear(hidden_size, n_class))
+        self.backbone = backbone
+        self.clf = nn.Sequential(*layers)
+
+    def forward(self, img):
+        embeds = self.backbone(img)
+        logits = self.clf(embeds)
+        return logits
