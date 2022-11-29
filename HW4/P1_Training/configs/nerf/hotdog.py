4b733fdccc604a32e3e6c314ea7da4dc301d4269
Li, Sheng-Wei <pha123661@gmail.com>
2022-11-29 13:22:16 +0800

    Update config

--- HW4/P1_Training/configs/nerf/hotdog.py
+++ HW4/P1_Training/configs/nerf/hotdog.py
@@ -4,7 +4,7 @@ expname = 'dvgo_hotdog'
 basedir = './logs/nerf_synthetic'
 
 data = dict(
-    datadir='./hw4_data/hotdog',
+    datadir='../hw4_data/hotdog',
     dataset_type='blender',
     white_bkgd=True,
 )
